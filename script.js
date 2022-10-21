50b0a9fd56d0f139b08f4c0f5347ce1db78c41ce
sammie-subham <116340385+sammie-subham@users.noreply.github.com>
2022-10-21 13:14:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+//         let weight='78';
+//         let height=1.69;
+//         let bmi1 = weight/height**2;
+//         console.log("Mark's BMI "+bmi1);
+//         weight=92;
+//         height=1.95;
+//         let bmi2 = weight/height**2;
+//         console.log("John's BMI "+bmi2);
+
+        
+    
+//    let age =2;
+//    const myname = "sam";
+// //    let sTemplate = `I'm ${myname} and i'm ${age} year old`;
+// //    console.log(sTemplate+`
+// //         gotcha
+// //         thanks`)
+// //  if(age===18){
+// //          console.log(`Here is your licience bud`)
+// //  } else if(age>=18){
+// //         console.log(`okay bud`)
+// //  } else{
+// //          console.log(`GO HOME!`)
+// //  }
+
+// if(bmi1>bmi2){
+//         console.log(`Mark's BMI is greater than John and it is ${(bmi1-bmi2)} greater`);
+// } else if(bmi2>bmi1){
+//         console.log(`John's BMI is greater than Mark and it is ${(bmi2-bmi1)} greater`);
+// }
+
+// console.log(String(23),12);
+// console.log(Number(age)+12);
+// console.log(Number(myname)+12);
+
+// console.log('23'+'45'+'56');
+// console.log('23'-'45'-'56');
+// console.log('23'*'45'*'56');
+// console.log('23'/'45');
+// console.log('23'>'45');
+
+let t = prompt("Type some thing");
+console.log(t);
